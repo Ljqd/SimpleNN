88197c1c782e7a3cf7e67561dc838ef1f3597b7a
Molochynskyi Yehor <molochynskyi.e@gmail.com>
2023-08-11 20:10:23 +0300

    Simple numeric test

---
+++ test_cases/numeric_binary_classification.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <cmath>
+#include <matplot/matplot.h>
+
+#include <Eigen/Dense>
+
+#include "../src/model/Sequential.h"
+
+#include "../src/layers/Dense.h"
+#include "../src/activation/Relu.h"
+#include "../src/activation/Tanh.h"
+#include "../src/activation/Sigmoid.h"
+#include "../src/cost/MeanSquareError.h"
+#include "../src/cost/BinaryCrossEntropy.h"
+
+void numeric_binary_classification();
