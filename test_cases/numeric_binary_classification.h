3f122fe5ac9a544862f94a6c284fd149010dd017
Molochynskyi Yehor <molochynskyi.e@gmail.com>
2023-08-13 12:24:13 +0300

    Update numeric test, add optimizer. Need to fix optimizer constructor and train method

--- test_cases/numeric_binary_classification.h
+++ test_cases/numeric_binary_classification.h
@@ -8,10 +8,14 @@
 #include "../src/model/Sequential.h"
 
 #include "../src/layers/Dense.h"
+
 #include "../src/activation/Relu.h"
 #include "../src/activation/Tanh.h"
 #include "../src/activation/Sigmoid.h"
+
 #include "../src/cost/MeanSquareError.h"
 #include "../src/cost/BinaryCrossEntropy.h"
+
+#include "../src/optimization/GradientDescent.h"
 
 void numeric_binary_classification();
